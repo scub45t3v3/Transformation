b9ad82a343d2e6149edbe34c2dc5bb16de51dd30
Steve Winberg <stevewin21@gmail.com>
2018-08-18 19:54:49 +0700

    refactor test/toWrap for readability

--- test/toWrap.js
+++ test/toWrap.js
@@ -1,50 +1,74 @@
-(function() {
-  var toWrap, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const toWrap = require('../toWrap');
 
-  toWrap = require('../toWrap');
+  // describe #toWrap
+  describe('#toWrap', () => {
+    it('should be a function', () => {
+      unit
+        .function(toWrap);
+    }); // end it
 
-  describe('#toWrap', function() {
-    it('should be a function', function() {
-      unit.function(toWrap);
-      return null;
-    });
-    it('should return a string of value when no options are provided', function() {
-      unit.string(toWrap('hello')).is('hello').string(toWrap(/asd/i)).is('/asd/i').string(toWrap(Symbol('no'))).is('Symbol(no)');
-      return null;
-    });
-    it('should return a string enclosing value with provided options', function() {
-      unit.string(toWrap('hello', {
-        start: '"',
-        end: '?"'
-      })).is('"hello?"').string(toWrap('tag', {
-        start: '<',
-        end: '>'
-      })).is('<tag>').string(toWrap('find me', {
-        start: '/',
-        end: '/i'
-      })).is('/find me/i').string(toWrap([1, 2, 3], {
-        start: '[',
-        end: ']'
-      })).is('[1,2,3]').string(toWrap([1, 2, 3], {
-        start: '(',
-        end: ')'
-      })).is('(1,2,3)');
-      return null;
-    });
-    return it('should return a string enclosing value with option provided when only 1 is set', function() {
-      unit.string(toWrap('hello', {
-        start: '"'
-      })).is('"hello"').string(toWrap('hello', {
-        end: '/'
-      })).is('/hello/').string(toWrap(true, {
-        start: '-'
-      })).is('-true-').string(toWrap('italics', {
-        end: '*'
-      })).is('*italics*');
-      return null;
-    });
-  });
+    it('should return a string of value when no options are provided', () => {
+      unit
+        .string(toWrap('hello'))
+        .is('hello')
+        .string(toWrap(/asd/i))
+        .is('/asd/i')
+        .string(toWrap(Symbol('no')))
+        .is('Symbol(no)');
+    }); // end it
 
-}).call(this);
+    it('should return a string enclosing value with provided options', () => {
+      unit
+        .string(toWrap('hello', {
+          start: '"',
+          end: '?"',
+        }))
+        .is('"hello?"')
+        .string(toWrap('tag', {
+          start: '<',
+          end: '>',
+        }))
+        .is('<tag>')
+        .string(toWrap('find me', {
+          start: '/',
+          end: '/i',
+        }))
+        .is('/find me/i')
+        .string(toWrap([1, 2, 3], {
+          start: '[',
+          end: ']',
+        }))
+        .is('[1,2,3]')
+        .string(toWrap([1, 2, 3], {
+          start: '(',
+          end: ')',
+        }))
+        .is('(1,2,3)');
+    }); // end it
+
+    it('should return a string enclosing value with option provided when only 1 is set', () => {
+      unit
+        .string(toWrap('hello', {
+          start: '"',
+        }))
+        .is('"hello"')
+        .string(toWrap('hello', {
+          end: '/',
+        }))
+        .is('/hello/')
+        .string(toWrap(true, {
+          start: '-',
+        }))
+        .is('-true-')
+        .string(toWrap('italics', {
+          end: '*',
+        }))
+        .is('*italics*');
+    }); // end it
+  }); // end describe #toWrap
+})(); // end IIFE
