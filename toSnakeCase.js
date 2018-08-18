71189ff9a0bce25286654ea72d16365c46cc1c6a
Steve Winberg <stevewin21@gmail.com>
2018-08-18 18:48:31 +0700

    refactor toSnakeCase for readability

--- toSnakeCase.js
+++ toSnakeCase.js
@@ -1,10 +1,17 @@
-(function() {
-  var toSnakeCase;
+'use strict';
 
-  toSnakeCase = function(value) {
-    return value != null ? typeof value.toString === "function" ? value.toString().trim().replace(/(?:\W|_)+/gm, '_').replace(/([a-z])([A-Z])/gm, '$1_$2').toLowerCase() : void 0 : void 0;
-  };
+(() => {
+  // include dependencies
+  const toString = require('./toString');
 
+  const toSnakeCase = (value) => {
+    return toString(value)
+      .trim()
+      .replace(/(?:\W|_)+/gm, '_')
+      .replace(/([a-z])([A-Z])/gm, '$1_$2')
+      .toLowerCase();
+  }; // end toSnakeCase
+
+  // export toSnakeCase as commonjs module
   module.exports = toSnakeCase;
-
-}).call(this);
+})(); // end IIFE
