9bd4b27604fe5b55bb29a423b7d0eda729575e24
Steve Winberg <stevewin21@gmail.com>
2018-08-18 19:05:02 +0700

    refactor test/index for readability

--- test/index.js
+++ test/index.js
@@ -1,19 +1,44 @@
-(function() {
-  var FUNCTIONS, index, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const index = require('../index');
+  const FUNCTIONS = [
+    'toAscii',
+    'toBoolean',
+    'toCamelCase',
+    'toCSV',
+    'toFormat',
+    'toJSON',
+    'toKebabCase',
+    'toLines',
+    'toList',
+    'toListSerial',
+    'toNumber',
+    'toPascalCase',
+    'toPlural',
+    'toRegExp',
+    'toSingular',
+    'toSlug',
+    'toSnakeCase',
+    'toString',
+    'toTokens',
+    'toWords',
+    'toWrap',
+    'toXML',
+    'toYAML',
+  ];
 
-  index = require('../index');
-
-  FUNCTIONS = ['toAscii', 'toBoolean', 'toCamelCase', 'toCSV', 'toFormat', 'toJSON', 'toKebabCase', 'toLines', 'toList', 'toListSerial', 'toNumber', 'toPascalCase', 'toPlural', 'toRegExp', 'toSingular', 'toSlug', 'toSnakeCase', 'toString', 'toTokens', 'toWords', 'toWrap', 'toXML', 'toYAML'];
-
-  describe('@scuba-squad/transformation', function() {
-    return it('should have several functions', function() {
-      unit.object(index).hasProperties(FUNCTIONS).matchEach(function(value, key) {
-        return typeof value === 'function' && /^to[A-Z]/.test(key);
-      });
-      return null;
-    });
-  });
-
-}).call(this);
+  // describe @scuba-squad/transformation
+  describe('@scuba-squad/transformation', () => {
+    it('should have several functions', () => {
+      unit
+        .object(index)
+        .hasProperties(FUNCTIONS)
+        .matchEach((value, key) => {
+          return typeof value === 'function' && /^to[A-Z]/.test(key);
+        });
+    }); // end it
+  }); // end describe @scuba-squad/transformation
+})(); // end IIFE
