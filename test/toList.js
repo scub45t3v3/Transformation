1ece9486f1b8b53e9837ce89f82abc01250e3413
Steve Winberg <stevewin21@gmail.com>
2018-08-18 19:25:58 +0700

    refactor test/toList for readability

--- test/toList.js
+++ test/toList.js
@@ -1,33 +1,44 @@
-(function() {
-  var toList, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const toList = require('../toList');
 
-  toList = require('../toList');
-
-  describe('#toList', function() {
-    it('should be a function', function() {
+  // describe #toList
+  describe('#toList', () => {
+    it('should be a function', () => {
       unit.function(toList);
-      return null;
-    });
-    it('should return a string  of value if only 1 value is provided', function() {
-      unit.string(toList([1])).is('1').string(toList('hi')).is('hi');
-      return null;
-    });
-    it('should return a comma seperated list with "and" as the last seperator when called with default options', function() {
-      unit.string(toList([1, 2, 3])).is('1, 2 and 3').string(toList(['big', 'tall'])).is('big and tall');
-      return null;
-    });
-    return it('should return a delimited list using provided options', function() {
-      unit.string(toList([127, 0, 0, 1], {
-        delimiter: '.',
-        last: '.'
-      })).is('127.0.0.1').string(toList([1, 0, 4, 'beta'], {
-        delimiter: '.',
-        last: '-'
-      })).is('1.0.4-beta');
-      return null;
-    });
-  });
+    }); // end it
 
-}).call(this);
+    it('should return a string  of value if only 1 value is provided', () => {
+      unit
+        .string(toList([1]))
+        .is('1')
+        .string(toList('hi'))
+        .is('hi');
+    }); // end it
+
+    it('should return a comma seperated list with "and" as the last seperator when called with default options', () => {
+      unit
+        .string(toList([1, 2, 3]))
+        .is('1, 2 and 3')
+        .string(toList(['big', 'tall']))
+        .is('big and tall');
+    }); // end it
+
+    it('should return a delimited list using provided options', () => {
+      unit
+        .string(toList([127, 0, 0, 1], {
+          delimiter: '.',
+          last: '.',
+        }))
+        .is('127.0.0.1')
+        .string(toList([1, 0, 4, 'beta'], {
+          delimiter: '.',
+          last: '-',
+        }))
+        .is('1.0.4-beta');
+    }); // end it
+  }); // end describe #toList
+})(); // end IIFE
