db520063714a57976da7aa4cbd108ca48825863e
Steve Winberg <stevewin21@gmail.com>
2018-08-18 19:51:38 +0700

    refactor test/toWords for readability

--- test/toWords.js
+++ test/toWords.js
@@ -1,19 +1,33 @@
-(function() {
-  var toWords, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const toWords = require('../toWords');
 
-  toWords = require('../toWords');
+  // describe #toWords
+  describe('#toWords', () => {
+    it('should be a function', () => {
+      unit
+        .function(toWords);
+    }); // end it
 
-  describe('#toWords', function() {
-    it('should be a function', function() {
-      unit.function(toWords);
-      return null;
-    });
-    return it('should return an array of words in a string', function() {
-      unit.array(toWords('hello world!')).is(['hello', 'world!']).array(toWords('123 Main St.')).is(['123', 'Main', 'St.']).array(toWords('this is $1.00')).is(['this', 'is', '$1.00']).array(toWords('a,b,c')).is(['a', 'b', 'c']).array(toWords('fruit: apples, oranges, bananas')).is(['fruit:', 'apples', 'oranges', 'bananas']).array(toWords('one')).is(['one']).array(toWords('')).is(['']);
-      return null;
-    });
-  });
-
-}).call(this);
+    it('should return an array of words in a string', () => {
+      unit
+        .array(toWords('hello world!'))
+        .is(['hello', 'world!'])
+        .array(toWords('123 Main St.'))
+        .is(['123', 'Main', 'St.'])
+        .array(toWords('this is $1.00'))
+        .is(['this', 'is', '$1.00'])
+        .array(toWords('a,b,c'))
+        .is(['a', 'b', 'c'])
+        .array(toWords('fruit: apples, oranges, bananas'))
+        .is(['fruit:', 'apples', 'oranges', 'bananas'])
+        .array(toWords('one'))
+        .is(['one'])
+        .array(toWords(''))
+        .is(['']);
+    }); // end it
+  }); // end describe #toWords
+})(); // end IIFE
