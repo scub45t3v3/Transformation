3dc07182bd7232acc820ec51bf4079d2ae65e4cf
Steve Winberg <stevewin21@gmail.com>
2018-08-18 19:12:20 +0700

    refactor test/toCSV for readability

--- test/toCSV.js
+++ test/toCSV.js
@@ -1,30 +1,32 @@
-(function() {
-  /*
-   * this is a direct passthrough to json2csv all tests should be preformed on
-   * json2csv this is only included to test the passthrough is setup as expected
-   */
-  var toCSV, unit;
+'use strict';
 
-  unit = require('unit.js');
+/*
+ * this is a direct passthrough to json2csv all tests should be preformed on
+ * json2csv this is only included to test the passthrough is setup as expected
+ */
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const toCSV = require('../toCSV');
 
-  toCSV = require('../toCSV');
+  describe('#toCSV', () => {
+    it('should be a function', () => {
+      unit
+        .function(toCSV);
+    }); // end it
 
-  describe('#toCSV', function() {
-    it('should be a function', function() {
-      unit.function(toCSV);
-      return null;
-    });
-    return it('should return a csv string for an object', function() {
-      var regex, test;
-      test = {
+    it('should return a csv string for an object', () => {
+      const test = {
         firstName: 'James',
         lastName: 'Bond',
-        age: 55
+        age: 55,
       };
-      regex = /^\s*"firstName"\s*,\s*"lastName"\s*,\s*"age"\s*"James"\s*,\s*"Bond"\s*,\s*55\s*$/m;
-      unit.string(toCSV(test)).match(regex);
-      return null;
-    });
-  });
 
-}).call(this);
+      const regex = /^\s*"firstName"\s*,\s*"lastName"\s*,\s*"age"\s*"James"\s*,\s*"Bond"\s*,\s*55\s*$/m;
+
+      unit
+        .string(toCSV(test))
+        .match(regex);
+    }); // end it
+  }); // end describe #toCSV
+})(); // end IIFE
