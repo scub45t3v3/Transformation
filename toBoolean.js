5be80bf429c201cc38e8edd891b4341ebc9a9875
Steve Winberg <stevewin21@gmail.com>
2018-08-18 17:59:48 +0700

    refactor toBoolean for readability

--- toBoolean.js
+++ toBoolean.js
@@ -1,22 +1,14 @@
-(function() {
-  var REGEX, toBoolean;
+'use strict';
 
-  REGEX = /^(?:0+|\-|null|undefined|NaN|off|f(?:alse)?|no(?:ne)?|neg(?:ative)?|)$/i; // 0
-  // -
-  // null
-  // undefined
-  // NaN
-  // off
-  // f or false
-  // no or none
-  // neg or negative
-  // empty string
+(() => {
+  // include dependencies
+  const toString = require('./toString');
+  const REGEX = /^(?:0+|-|null|undefined|NaN|off|f(?:alse)?|no(?:ne)?|neg(?:ative)?|)$/i;
 
-  toBoolean = function(value) {
-    var ref;
-    return !!value && !REGEX.test((ref = (value != null ? typeof value.toString === "function" ? value.toString() : void 0 : void 0) || `${value}`) != null ? typeof ref.trim === "function" ? ref.trim() : void 0 : void 0);
+  const toBoolean = (value) => {
+    return !!value && !REGEX.test(toString(value).trim());
   };
 
+  // export toBoolean as commonjs module
   module.exports = toBoolean;
-
-}).call(this);
+})(); // end IIFE
