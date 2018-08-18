15c59cedcb886693077ce00d77bc7b08f4b4a4bd
Steve Winberg <stevewin21@gmail.com>
2018-08-18 19:14:35 +0700

    refactor test/toCamelCase for readability

--- test/toCamelCase.js
+++ test/toCamelCase.js
@@ -1,39 +1,123 @@
-(function() {
-  var toCamelCase, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const toCamelCase = require('../toCamelCase');
 
-  toCamelCase = require('../toCamelCase');
+  // describe #toCamelCase
+  describe('#toCamelCase', () => {
+    it('should be a function', () => {
+      unit
+        .function(toCamelCase);
+    }); // end it
 
-  describe('#toCamelCase', function() {
-    it('should be a function', function() {
-      unit.function(toCamelCase);
-      return null;
-    });
-    it('should return camelCase for camelCase strings', function() {
-      unit.string(toCamelCase('helloWorld')).is('helloWorld').string(toCamelCase('person')).is('person').string(toCamelCase('scubaSquad')).is('scubaSquad').string(toCamelCase('json2Xml')).is('json2Xml').string(toCamelCase('json2XML')).is('json2XML');
-      return null;
-    });
-    it('should return camelCase for PascalCase strings', function() {
-      unit.string(toCamelCase('HelloWorld')).is('helloWorld').string(toCamelCase('Person')).is('person').string(toCamelCase('ScubaSquad')).is('scubaSquad').string(toCamelCase('Json2Xml')).is('json2Xml').string(toCamelCase('Json2xml')).is('json2xml');
-      return null;
-    });
-    it('should return camelCase for snake_case strings', function() {
-      unit.string(toCamelCase('hello_world')).is('helloWorld').string(toCamelCase('_person')).is('person').string(toCamelCase('scuba_squad')).is('scubaSquad').string(toCamelCase('json_2_xml')).is('json2Xml').string(toCamelCase('hello__world')).is('helloWorld').string(toCamelCase('_person_')).is('person').string(toCamelCase('_scuba_squad_')).is('scubaSquad').string(toCamelCase('json_2_XML')).is('json2XML').string(toCamelCase('_Hello__World_')).is('helloWorld').string(toCamelCase('__Person__')).is('person').string(toCamelCase('scuba______squad')).is('scubaSquad').string(toCamelCase('JSON_2_XML')).is('jSON2XML');
-      return null;
-    });
-    it('should return camelCase for kebab-case strings', function() {
-      unit.string(toCamelCase('hello-world')).is('helloWorld').string(toCamelCase('-person')).is('person').string(toCamelCase('scuba-squad')).is('scubaSquad').string(toCamelCase('json-2-xml')).is('json2Xml').string(toCamelCase('hello--world')).is('helloWorld').string(toCamelCase('-person-')).is('person').string(toCamelCase('-scuba-squad-')).is('scubaSquad').string(toCamelCase('-json-2-xml')).is('json2Xml').string(toCamelCase('-hello- -world-')).is('helloWorld').string(toCamelCase('-person- -')).is('person').string(toCamelCase('scuba------squad')).is('scubaSquad').string(toCamelCase('JSON-2-XML')).is('jSON2XML');
-      return null;
-    });
-    it('should return camelCase for delimited strings', function() {
-      unit.string(toCamelCase('hello world')).is('helloWorld').string(toCamelCase('is,person')).is('isPerson').string(toCamelCase('@scuba-squad/transformation:toCamelCase')).is('scubaSquadTransformationToCamelCase');
-      return null;
-    });
-    return it('should return camelCase for arrays', function() {
-      unit.string(toCamelCase(['hello', 'world'])).is('helloWorld').string(toCamelCase(['-', 'person'])).is('person').string(toCamelCase(['scuba', 'squad'])).is('scubaSquad').string(toCamelCase(['json', 2, 'xml'])).is('json2Xml').string(toCamelCase(['json', 2, 'XML'])).is('json2XML');
-      return null;
-    });
-  });
+    it('should return camelCase for camelCase strings', () => {
+      unit
+        .string(toCamelCase('helloWorld'))
+        .is('helloWorld')
+        .string(toCamelCase('person'))
+        .is('person')
+        .string(toCamelCase('scubaSquad'))
+        .is('scubaSquad')
+        .string(toCamelCase('json2Xml'))
+        .is('json2Xml')
+        .string(toCamelCase('json2XML'))
+        .is('json2XML');
+    }); // end it
 
-}).call(this);
+    it('should return camelCase for PascalCase strings', () => {
+      unit
+        .string(toCamelCase('HelloWorld'))
+        .is('helloWorld')
+        .string(toCamelCase('Person'))
+        .is('person')
+        .string(toCamelCase('ScubaSquad'))
+        .is('scubaSquad')
+        .string(toCamelCase('Json2Xml'))
+        .is('json2Xml')
+        .string(toCamelCase('Json2xml'))
+        .is('json2xml');
+    }); // end it
+
+    it('should return camelCase for snake_case strings', () => {
+      unit
+        .string(toCamelCase('hello_world'))
+        .is('helloWorld')
+        .string(toCamelCase('_person'))
+        .is('person')
+        .string(toCamelCase('scuba_squad'))
+        .is('scubaSquad')
+        .string(toCamelCase('json_2_xml'))
+        .is('json2Xml')
+        .string(toCamelCase('hello__world'))
+        .is('helloWorld')
+        .string(toCamelCase('_person_'))
+        .is('person')
+        .string(toCamelCase('_scuba_squad_'))
+        .is('scubaSquad')
+        .string(toCamelCase('json_2_XML'))
+        .is('json2XML')
+        .string(toCamelCase('_Hello__World_'))
+        .is('helloWorld')
+        .string(toCamelCase('__Person__'))
+        .is('person')
+        .string(toCamelCase('scuba______squad'))
+        .is('scubaSquad')
+        .string(toCamelCase('JSON_2_XML'))
+        .is('jSON2XML');
+    }); // end it
+
+    it('should return camelCase for kebab-case strings', () => {
+      unit
+        .string(toCamelCase('hello-world'))
+        .is('helloWorld')
+        .string(toCamelCase('-person'))
+        .is('person')
+        .string(toCamelCase('scuba-squad'))
+        .is('scubaSquad')
+        .string(toCamelCase('json-2-xml'))
+        .is('json2Xml')
+        .string(toCamelCase('hello--world'))
+        .is('helloWorld')
+        .string(toCamelCase('-person-'))
+        .is('person')
+        .string(toCamelCase('-scuba-squad-'))
+        .is('scubaSquad')
+        .string(toCamelCase('-json-2-xml'))
+        .is('json2Xml')
+        .string(toCamelCase('-hello- -world-'))
+        .is('helloWorld')
+        .string(toCamelCase('-person- -'))
+        .is('person')
+        .string(toCamelCase('scuba------squad'))
+        .is('scubaSquad')
+        .string(toCamelCase('JSON-2-XML'))
+        .is('jSON2XML');
+    }); // end it
+
+    it('should return camelCase for delimited strings', () => {
+      unit
+        .string(toCamelCase('hello world'))
+        .is('helloWorld')
+        .string(toCamelCase('is,person'))
+        .is('isPerson')
+        .string(toCamelCase('@scuba-squad/transformation:toCamelCase'))
+        .is('scubaSquadTransformationToCamelCase');
+    }); // end it
+
+    it('should return camelCase for arrays', () => {
+      unit
+        .string(toCamelCase(['hello', 'world']))
+        .is('helloWorld')
+        .string(toCamelCase(['-', 'person']))
+        .is('person')
+        .string(toCamelCase(['scuba', 'squad']))
+        .is('scubaSquad')
+        .string(toCamelCase(['json', 2, 'xml']))
+        .is('json2Xml')
+        .string(toCamelCase(['json', 2, 'XML']))
+        .is('json2XML');
+    }); // end it
+  }); // end describe 3toCamelCase
+})(); // end IIFE
