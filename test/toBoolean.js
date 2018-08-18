5b19b443c708b0ecb27e5a4d086e722662be0ad3
Steve Winberg <stevewin21@gmail.com>
2018-08-18 19:08:15 +0700

    refactor test/toBoolean for readability

--- test/toBoolean.js
+++ test/toBoolean.js
@@ -1,65 +1,162 @@
-(function() {
-  var toBoolean, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const toBoolean = require('../toBoolean');
 
-  toBoolean = require('../toBoolean');
+  // describe #toBoolean
+  describe('#toBoolean', () => {
+    it('should retun true for true booleans', () => {
+      unit
+        .bool(toBoolean(true))
+        .isTrue()
+        .bool(toBoolean(new Boolean(1)))
+        .isTrue();
+    }); // end it
 
-  describe('#toBoolean', function() {
-    it('should retun true for true booleans', function() {
-      unit.bool(toBoolean(true)).isTrue().bool(toBoolean(new Boolean(1))).isTrue();
-      return null;
-    });
-    it('should return true for non falsey strings', function() {
-      unit.bool(toBoolean('true')).isTrue().bool(toBoolean('1')).isTrue().bool(toBoolean('a')).isTrue().bool(toBoolean('hello')).isTrue().bool(toBoolean('  hi   ')).isTrue().bool(toBoolean('+')).isTrue();
-      return null;
-    });
-    it('should return true to numbers != 0', function() {
-      unit.bool(toBoolean(1)).isTrue().bool(toBoolean(-1)).isTrue().bool(toBoolean(Number.MAX_VALUE)).isTrue().bool(toBoolean(Number.MIN_VALUE)).isTrue();
-      return null;
-    });
-    it('should return true for Infinity', function() {
-      unit.bool(toBoolean(2e308)).isTrue().bool(toBoolean(-2e308)).isTrue();
-      return null;
-    });
-    it('should return true for arrays that don\'t stringify to falsy strings', function() {
-      unit.bool(toBoolean([1, 2])).isTrue().bool(toBoolean(['a'])).isTrue().bool(toBoolean([0, 0, 3])).isTrue();
-      return null;
-    });
-    it('should return true for objects that don\'t stringify to fasly strings', function() {
-      unit.bool(toBoolean({})).isTrue().bool(toBoolean({
-        a: 5
-      })).isTrue().bool(toBoolean(new Set([1, 2]))).isTrue().bool(toBoolean(new String(':'))).isTrue().bool(toBoolean(new Date())).isTrue().bool(toBoolean(new Array(1, 2))).isTrue().bool(toBoolean(new RegExp())).isTrue();
-      return null;
-    });
-    it('should return false for false booleans', function() {
-      unit.bool(toBoolean(false)).isFalse().bool(toBoolean(new Boolean(0))).isFalse();
-      return null;
-    });
-    it('should return false for falsy strings', function() {
-      unit.bool(toBoolean('')).isFalse().bool(toBoolean('     ')).isFalse().bool(toBoolean('\n')).isFalse().bool(toBoolean('\r')).isFalse().bool(toBoolean('\v')).isFalse().bool(toBoolean('0')).isFalse().bool(toBoolean('false')).isFalse().bool(toBoolean('f')).isFalse().bool(toBoolean('off')).isFalse().bool(toBoolean('no')).isFalse().bool(toBoolean('none')).isFalse().bool(toBoolean('neg')).isFalse().bool(toBoolean('negative')).isFalse().bool(toBoolean('-')).isFalse();
-      return null;
-    });
-    it('should return false for 0', function() {
-      unit.bool(toBoolean(0)).isFalse();
-      return null;
-    });
-    it('should return false for arrays that stringify to falsy strings', function() {
-      unit.bool(toBoolean([])).isFalse().bool(toBoolean([0])).isFalse().bool(toBoolean([false])).isFalse().bool(toBoolean(['NO'])).isFalse();
-      return null;
-    });
-    it('should return false for NaN', function() {
-      unit.bool(toBoolean(0/0)).isFalse();
-      return null;
-    });
-    it('should return false for null', function() {
-      unit.bool(toBoolean(null)).isFalse();
-      return null;
-    });
-    return it('should return false for undefined', function() {
-      unit.bool(toBoolean()).isFalse().bool(toBoolean(void 0)).isFalse();
-      return null;
-    });
-  });
+    it('should return true for non falsey strings', () => {
+      unit
+        .bool(toBoolean('true'))
+        .isTrue()
+        .bool(toBoolean('1'))
+        .isTrue()
+        .bool(toBoolean('a'))
+        .isTrue()
+        .bool(toBoolean('hello'))
+        .isTrue()
+        .bool(toBoolean('  hi   '))
+        .isTrue()
+        .bool(toBoolean('+'))
+        .isTrue();
+    }); // end it
 
-}).call(this);
+    it('should return true to numbers != 0', () => {
+      unit
+        .bool(toBoolean(1))
+        .isTrue()
+        .bool(toBoolean(-1))
+        .isTrue()
+        .bool(toBoolean(Number.MAX_VALUE))
+        .isTrue()
+        .bool(toBoolean(Number.MIN_VALUE))
+        .isTrue();
+    }); // end it
+
+    it('should return true for Infinity', () => {
+      unit
+        .bool(toBoolean(2e308))
+        .isTrue()
+        .bool(toBoolean(-2e308))
+        .isTrue();
+    }); // end it
+
+    it('should return true for arrays that don\'t stringify to falsy strings', () => {
+      unit
+        .bool(toBoolean([1, 2]))
+        .isTrue()
+        .bool(toBoolean(['a']))
+        .isTrue()
+        .bool(toBoolean([0, 0, 3]))
+        .isTrue();
+    }); // end it
+
+    it('should return true for objects that don\'t stringify to fasly strings', () => {
+      unit
+        .bool(toBoolean({}))
+        .isTrue()
+        .bool(toBoolean({
+          a: 5,
+        }))
+        .isTrue()
+        .bool(toBoolean(new Set([1, 2])))
+        .isTrue()
+        .bool(toBoolean(new String(':')))
+        .isTrue()
+        .bool(toBoolean(new Date()))
+        .isTrue()
+        .bool(toBoolean(new Array(1, 2)))
+        .isTrue()
+        .bool(toBoolean(new RegExp()))
+        .isTrue();
+    }); // end it
+
+    it('should return false for false booleans', () => {
+      unit
+        .bool(toBoolean(false))
+        .isFalse()
+        .bool(toBoolean(new Boolean(0)))
+        .isFalse();
+    }); // end it
+
+    it('should return false for falsy strings', () => {
+      unit
+        .bool(toBoolean(''))
+        .isFalse()
+        .bool(toBoolean('     '))
+        .isFalse()
+        .bool(toBoolean('\n'))
+        .isFalse()
+        .bool(toBoolean('\r'))
+        .isFalse()
+        .bool(toBoolean('\v'))
+        .isFalse()
+        .bool(toBoolean('0'))
+        .isFalse()
+        .bool(toBoolean('false'))
+        .isFalse()
+        .bool(toBoolean('f'))
+        .isFalse()
+        .bool(toBoolean('off'))
+        .isFalse()
+        .bool(toBoolean('no'))
+        .isFalse()
+        .bool(toBoolean('none'))
+        .isFalse()
+        .bool(toBoolean('neg'))
+        .isFalse()
+        .bool(toBoolean('negative'))
+        .isFalse()
+        .bool(toBoolean('-'))
+        .isFalse();
+    }); // end it
+
+    it('should return false for 0', () => {
+      unit
+        .bool(toBoolean(0))
+        .isFalse();
+    }); // end it
+
+    it('should return false for arrays that stringify to falsy strings', () => {
+      unit
+        .bool(toBoolean([]))
+        .isFalse()
+        .bool(toBoolean([0]))
+        .isFalse()
+        .bool(toBoolean([false]))
+        .isFalse()
+        .bool(toBoolean(['NO']))
+        .isFalse();
+    }); // end it
+
+    it('should return false for NaN', () => {
+      unit
+        .bool(toBoolean(0 / 0))
+        .isFalse();
+    }); // end it
+
+    it('should return false for null', () => {
+      unit
+        .bool(toBoolean(null))
+        .isFalse();
+    }); // end it
+
+    it('should return false for undefined', () => {
+      unit
+        .bool(toBoolean())
+        .isFalse()
+        .bool(toBoolean(undefined))
+        .isFalse();
+    }); // end it
+  }); // end describe #toBoolean
+})(); // end IIFE
