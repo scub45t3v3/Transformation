e18633bc02b6b8198e1cc4f27d302582a4208d4d
Steve Winberg <stevewin21@gmail.com>
2018-08-18 19:19:52 +0700

    refactor test/toFormat for readability

--- test/toFormat.js
+++ test/toFormat.js
@@ -1,26 +1,31 @@
-(function() {
-  /*
-   * this is a direct passthrough to util.format all tests should be preformed
-   * on util.format this is only included to test the passthrough is setup as
-   * expected
-   */
-  var toFormat, unit;
+'use strict';
 
-  unit = require('unit.js');
+/*
+ * this is a direct passthrough to util.format all tests should be preformed
+ * on util.format this is only included to test the passthrough is setup as
+ * expected
+ */
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const toFormat = require('../toFormat');
 
-  toFormat = require('../toFormat');
+  describe('#toFormat', () => {
+    it('should be a function', () => {
+      unit
+        .function(toFormat);
+    }); // end it
 
-  describe('#toFormat', function() {
-    it('should be a function', function() {
-      unit.function(toFormat);
-      return null;
-    });
-    return it('should return a string formated print-f style', function() {
-      unit.string(toFormat('%s, %s', 'Bond', 'James')).is('Bond, James').string(toFormat('%d-%d-%d', 1963, 12, 31)).is('1963-12-31').string(toFormat('$> %j', {
-        a: 5
-      })).match(/^\$>\s*{\s*"a"\s*:\s*5\s*}\s*$/);
-      return null;
-    });
-  });
-
-}).call(this);
+    it('should return a string formated print-f style', () => {
+      unit
+        .string(toFormat('%s, %s', 'Bond', 'James'))
+        .is('Bond, James')
+        .string(toFormat('%d-%d-%d', 1963, 12, 31))
+        .is('1963-12-31')
+        .string(toFormat('$> %j', {
+          a: 5,
+        }))
+        .match(/^\$>\s*{\s*"a"\s*:\s*5\s*}\s*$/);
+    }); // end it
+  }); // end describe #toFormat
+})(); // end IIFE
