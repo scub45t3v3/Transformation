e7895f32c9f47e3b08f5b06be998a47621815dfb
Steve Winberg <stevewin21@gmail.com>
2018-08-18 19:39:54 +0700

    refactor test/toSingular for readability

--- test/toSingular.js
+++ test/toSingular.js
@@ -1,23 +1,31 @@
-(function() {
-  /*
-   * this is a direct passthrough to pluralize all tests should be preformed on
-   * pluralize this is only included to test the passthrough is setup as expected
-   */
-  var toSingular, unit;
+'use strict';
 
-  unit = require('unit.js');
+/*
+ * this is a direct passthrough to pluralize all tests should be preformed on
+ * pluralize this is only included to test the passthrough is setup as
+ * expected
+ */
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const toSingular = require('../toSingular');
 
-  toSingular = require('../toSingular');
+  describe('#toSingular', () => {
+    it('should be a function', () => {
+      unit
+        .function(toSingular);
+    }); // end it
 
-  describe('#toSingular', function() {
-    it('should be a function', function() {
-      unit.function(toSingular);
-      return null;
-    });
-    return it('should return a singular version of a word', function() {
-      unit.string(toSingular('tables')).is('table').string(toSingular('chairs')).is('chair').string(toSingular('cars')).is('car').string(toSingular('keys')).is('key');
-      return null;
-    });
-  });
-
-}).call(this);
+    it('should return a singular version of a word', () => {
+      unit
+        .string(toSingular('tables'))
+        .is('table')
+        .string(toSingular('chairs'))
+        .is('chair')
+        .string(toSingular('cars'))
+        .is('car')
+        .string(toSingular('keys'))
+        .is('key');
+    }); // end it
+  }); // end describe #toSingular
+})(); // end IIFE
