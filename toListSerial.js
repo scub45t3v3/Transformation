b7f7a05c196ea8d8cf2021c6c011c409765e9c82
Steve Winberg <stevewin21@gmail.com>
2018-11-03 14:20:09 +0700

    add debug to toListSerial

--- toListSerial.js
+++ toListSerial.js
@@ -2,9 +2,11 @@
 
 (() => {
   // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toListSerial');
   const toList = require('./toList');
 
   const toListSerial = (value, opt = {}) => {
+    debug('call:toListSerial(%o, %o)', value, opt);
     opt = Object.assign({
       delimiter: ', ',
       last: ' and ',
