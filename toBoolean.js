38de30b1b6d88578ae7c2778d4a934a51c710885
Steve Winberg <stevewin21@gmail.com>
2018-11-03 13:41:38 +0700

    add debug to toBoolean

--- toBoolean.js
+++ toBoolean.js
@@ -2,10 +2,13 @@
 
 (() => {
   // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toBoolean');
   const toString = require('./toString');
   const REGEX = /^(?:0+|-|null|undefined|NaN|off|f(?:alse)?|no(?:ne)?|neg(?:ative)?|)$/i;
 
   const toBoolean = (value) => {
+    debug('call:toBoolean(%o)', value);
+
     return !!value && !REGEX.test(toString(value).trim());
   };
 
