9ad389d7bcad7047bd333c4f83d79fe597484a52
Steve Winberg <stevewin21@gmail.com>
2018-11-03 14:25:16 +0700

    add debug to toMD5

--- toMD5.js
+++ toMD5.js
@@ -2,9 +2,12 @@
 
 (() => {
   // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toMD5');
   const crypto = require('crypto');
 
   const toMD5 = (value) => {
+    debug('call:toMD5(%o)', value);
+
     if (!(value instanceof String || typeof value === 'string')) {
       value = JSON.stringify(value);
     }
