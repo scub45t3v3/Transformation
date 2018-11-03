906f65d0d9db1906a012835fd2bca31365bdcda8
Steve Winberg <stevewin21@gmail.com>
2018-11-03 13:43:32 +0700

    add debug to toCSV

--- toCSV.js
+++ toCSV.js
@@ -2,9 +2,12 @@
 
 (() => {
   // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toCSV');
   const {parse} = require('json2csv');
 
   const toCSV = (value, opt = {}) => {
+    debug('call:toCSV(%o, %o)', value, opt);
+
     return parse(value, opt);
   };
 
