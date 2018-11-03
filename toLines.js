86d24419d7cbfb194b5a02590efa04b62ad7d258
Steve Winberg <stevewin21@gmail.com>
2018-11-03 14:06:00 +0700

    add debug to toLines

--- toLines.js
+++ toLines.js
@@ -2,9 +2,12 @@
 
 (() => {
   // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toLines');
   const toTokens = require('./toTokens');
 
   const toLines = (value) => {
+    debug('call:toLines(%o)', value);
+
     return toTokens(value, /\r\n?|\n/gm);
   };
 
