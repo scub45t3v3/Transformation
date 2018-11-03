6a0de4ec62a29057b52c7fb2a2555c3dd87734f3
Steve Winberg <stevewin21@gmail.com>
2018-11-03 15:18:34 +0700

    add debug to toWords

--- toWords.js
+++ toWords.js
@@ -2,9 +2,12 @@
 
 (() => {
   // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toWords');
   const toTokens = require('./toTokens');
 
   const toWords = (value) => {
+    debug('call:toWords(%o)', value);
+
     return toTokens(value, /[!?.:;]*[\s,]+/);
   }; // end toWords
 
