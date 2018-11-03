7ebaeaff21756e231e52fe43217682bbe13f6f1c
Steve Winberg <stevewin21@gmail.com>
2018-11-03 14:30:29 +0700

    add debug to toNumber

--- toNumber.js
+++ toNumber.js
@@ -1,7 +1,12 @@
 'use strict';
 
 (() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toNumber');
+
   const toNumber = (value) => {
+    debug('call:toNumber(%o)', value);
+
     return Number(value);
   }; // end toNumber
 
