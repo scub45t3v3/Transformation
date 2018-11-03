2875dc523617d9a0c4b5fdfe26aecc2f068f09d7
Steve Winberg <stevewin21@gmail.com>
2018-11-03 13:52:44 +0700

    add debug to toFormat

--- toFormat.js
+++ toFormat.js
@@ -2,9 +2,12 @@
 
 (() => {
   // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toFormat');
   const {format} = require('util');
 
   const toFormat = (...args) => {
+    debug('call:toFormat(%o)', args);
+
     return format(...args);
   };
 
