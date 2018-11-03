9ddcd429e7360b35d0c09210554507f79656ed80
Steve Winberg <stevewin21@gmail.com>
2018-11-03 13:55:45 +0700

    add debug to toJSON

--- toJSON.js
+++ toJSON.js
@@ -1,7 +1,12 @@
 'use strict';
 
 (() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toJSON');
+
   const toJSON = (value, space = 2) => {
+    debug('call:toJSON(%o, %o)', value, space);
+
     return JSON.stringify(value, null, space);
   };
 
