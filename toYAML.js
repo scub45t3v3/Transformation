a89030199d7d11597110feba9c3a335547b19282
Steve Winberg <stevewin21@gmail.com>
2018-11-03 15:34:04 +0700

    add debug to toYAML

--- toYAML.js
+++ toYAML.js
@@ -2,9 +2,15 @@
 
 (() => {
   // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toYAML');
   const yaml = require('js-yaml');
 
   const toYAML = (value, opt = {}) => {
+    debug('call:toYAML(%o, %o)', value, opt);
+    opt = Object.assign({
+      skipInvalid: true,
+    }, opt);
+
     return yaml.safeDump(value, opt);
   }; // end toYAML
 
