f6042a34f29a68de1572194e911543373b3f1e92
Steve Winberg <stevewin21@gmail.com>
2018-11-03 15:03:02 +0700

    add debug to toSnakeCase

--- toSnakeCase.js
+++ toSnakeCase.js
@@ -2,9 +2,12 @@
 
 (() => {
   // include dependencies
+  const debug = require('debug')('@scuba-squad:transformation:toSnakeCase');
   const toString = require('./toString');
 
   const toSnakeCase = (value) => {
+    debug('call:toSnakeCase(%o)', value);
+
     return toString(value)
       .trim()
       .replace(/(?:\W|_)+/gm, '_')
