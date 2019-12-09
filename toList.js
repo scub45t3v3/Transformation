009e2c6a1b72ef35063604ec319cfc1b7f9b0100
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:18:10 +0700

    remove iife from toList.js

--- toList.js
+++ toList.js
@@ -1,30 +1,28 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toList');
-  const {flatten} = require('underscore');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toList');
+const {flatten} = require('underscore');
 
-  const toList = (value, opt = {}) => {
-    debug('call:toList(%o, %o)', value, opt);
-    opt = Object.assign({
-      delimiter: ', ',
-      last: ' and ',
-    }, opt);
+const toList = (value, opt = {}) => {
+  debug('call:toList(%o, %o)', value, opt);
+  opt = Object.assign({
+    delimiter: ', ',
+    last: ' and ',
+  }, opt);
 
-    value = flatten([value]).filter((v) => {
-      return !(v == null || v === '' || Number.isNaN(v));
-    });
+  value = flatten([value]).filter((v) => {
+    return !(v == null || v === '' || Number.isNaN(v));
+  });
 
-    const last = value.pop();
+  const last = value.pop();
 
-    if (!value.length) {
-      return `${last}`;
-    }
+  if (!value.length) {
+    return `${last}`;
+  }
 
-    return `${value.join(opt.delimiter)}${opt.last}${last}`;
-  }; // end toList
+  return `${value.join(opt.delimiter)}${opt.last}${last}`;
+}; // end toList
 
-  // export toList as commonjs module
-  module.exports = toList;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toList;
