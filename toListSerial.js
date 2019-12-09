e0859abe0a3f341a581dad0d6c6427d36289f01a
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:18:23 +0700

    remove iife from toListSerial.js

--- toListSerial.js
+++ toListSerial.js
@@ -1,22 +1,20 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toListSerial');
-  const toList = require('./toList');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toListSerial');
+const toList = require('./toList');
 
-  const toListSerial = (value, opt = {}) => {
-    debug('call:toListSerial(%o, %o)', value, opt);
-    opt = Object.assign({
-      delimiter: ', ',
-      last: ' and ',
-    }, opt);
+const toListSerial = (value, opt = {}) => {
+  debug('call:toListSerial(%o, %o)', value, opt);
+  opt = Object.assign({
+    delimiter: ', ',
+    last: ' and ',
+  }, opt);
 
-    opt.last = `${opt.delimiter}${opt.last}`.replace(/\s+/, ' ');
+  opt.last = `${opt.delimiter}${opt.last}`.replace(/\s+/, ' ');
 
-    return toList(value, opt);
-  }; // end toListSerial
+  return toList(value, opt);
+}; // end toListSerial
 
-  // export toListSerial as commonjs module
-  module.exports = toListSerial;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toListSerial;
