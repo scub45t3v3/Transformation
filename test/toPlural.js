5f1cba22b8bdbb74a0d8063a121309741770d72d
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:05:22 +0700

    remove iife from test/toPlural.js

--- test/toPlural.js
+++ test/toPlural.js
@@ -5,28 +5,26 @@
  * pluralize this is only included to test the passthrough is setup as
  * expected
  */
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const toPlural = require('../toPlural');
+// include dependencies
+const unit = require('unit.js');
+const toPlural = require('../toPlural');
 
-  // describe #toPlural
-  describe('#toPlural', () => {
-    it('should be a function', () => {
-      unit
-        .function(toPlural);
-    }); // end it
+// describe #toPlural
+describe('#toPlural', () => {
+  it('should be a function', () => {
+    unit
+      .function(toPlural);
+  }); // end it
 
-    it('should return a plural version of a word', () => {
-      unit
-        .string(toPlural('table'))
-        .is('tables')
-        .string(toPlural('chair'))
-        .is('chairs')
-        .string(toPlural('car'))
-        .is('cars')
-        .string(toPlural('key'))
-        .is('keys');
-    }); // end it
-  }); // end describe #toPlural
-})(); // end IIFE
+  it('should return a plural version of a word', () => {
+    unit
+      .string(toPlural('table'))
+      .is('tables')
+      .string(toPlural('chair'))
+      .is('chairs')
+      .string(toPlural('car'))
+      .is('cars')
+      .string(toPlural('key'))
+      .is('keys');
+  }); // end it
+}); // end describe #toPlural
