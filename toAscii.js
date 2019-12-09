c2a40180101879e03c223181d462660a0ad3c44d
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:16:48 +0700

    remove iife from toAscii.js

--- toAscii.js
+++ toAscii.js
@@ -1,33 +1,31 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toAscii');
-  const currencies = require('./util/currencies');
-  const diacritics = require('./util/diacritics');
-  const symbols = require('./util/symbols');
-  const toString = require('./toString');
-  const charMap = Object.assign({}, currencies, diacritics, symbols);
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toAscii');
+const currencies = require('./util/currencies');
+const diacritics = require('./util/diacritics');
+const symbols = require('./util/symbols');
+const toString = require('./toString');
+const charMap = Object.assign({}, currencies, diacritics, symbols);
 
-  const toAscii = (value) => {
-    debug('call:toAscii(%o)', value);
+const toAscii = (value) => {
+  debug('call:toAscii(%o)', value);
 
-    if (Array.isArray(value)) {
-      value = value.join(' ');
-    }
+  if (Array.isArray(value)) {
+    value = value.join(' ');
+  }
 
-    return toString(value)
-      .split('')
-      .map((char) => {
-        if (char.codePointAt(0) > 128) {
-          char = charMap[char] || '';
-        }
+  return toString(value)
+    .split('')
+    .map((char) => {
+      if (char.codePointAt(0) > 128) {
+        char = charMap[char] || '';
+      }
 
-        return char;
-      })
-      .join('');
-  }; // end toAscii
+      return char;
+    })
+    .join('');
+}; // end toAscii
 
-  // export toAscii as commonjs module
-  module.exports = toAscii;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toAscii;
