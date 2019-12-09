c0903f881445935b2f39f9ec23472c9bade16241
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:03:45 +0700

    remove iife from test/toJSON.js

--- test/toJSON.js
+++ test/toJSON.js
@@ -5,29 +5,27 @@
  * preformed on JSON.stringify this is only included to test the passthrough
  * is setup as expected
  */
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const toJSON = require('../toJSON');
+// include dependencies
+const unit = require('unit.js');
+const toJSON = require('../toJSON');
 
-  describe('#toJSON', () => {
-    it('should be a function', () => {
-      unit
-        .function(toJSON);
-    }); // end it
+describe('#toJSON', () => {
+  it('should be a function', () => {
+    unit
+      .function(toJSON);
+  }); // end it
 
-    it('should return a json string for an object', () => {
-      const test = {
-        firstName: 'James',
-        lastName: 'Bond',
-        age: 55,
-      };
+  it('should return a json string for an object', () => {
+    const test = {
+      firstName: 'James',
+      lastName: 'Bond',
+      age: 55,
+    };
 
-      const regex = /^\s*{\s*"firstName"\s*:\s*"James"\s*,\s*"lastName"\s*:\s*"Bond"\s*,\s*"age"\s*:\s*55\s*}\s*$/m;
+    const regex = /^\s*{\s*"firstName"\s*:\s*"James"\s*,\s*"lastName"\s*:\s*"Bond"\s*,\s*"age"\s*:\s*55\s*}\s*$/m;
 
-      unit
-        .string(toJSON(test))
-        .match(regex);
-    }); // end it
-  }); // end describe #toJSON
-})(); // end IIFE
+    unit
+      .string(toJSON(test))
+      .match(regex);
+  }); // end it
+}); // end describe #toJSON
