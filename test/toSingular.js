10d63c3778c135b50dd5b2a8e5472570e734b61c
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:05:59 +0700

    remove iife from test/toSingular.js

--- test/toSingular.js
+++ test/toSingular.js
@@ -5,27 +5,25 @@
  * pluralize this is only included to test the passthrough is setup as
  * expected
  */
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const toSingular = require('../toSingular');
+// include dependencies
+const unit = require('unit.js');
+const toSingular = require('../toSingular');
 
-  describe('#toSingular', () => {
-    it('should be a function', () => {
-      unit
-        .function(toSingular);
-    }); // end it
+describe('#toSingular', () => {
+  it('should be a function', () => {
+    unit
+      .function(toSingular);
+  }); // end it
 
-    it('should return a singular version of a word', () => {
-      unit
-        .string(toSingular('tables'))
-        .is('table')
-        .string(toSingular('chairs'))
-        .is('chair')
-        .string(toSingular('cars'))
-        .is('car')
-        .string(toSingular('keys'))
-        .is('key');
-    }); // end it
-  }); // end describe #toSingular
-})(); // end IIFE
+  it('should return a singular version of a word', () => {
+    unit
+      .string(toSingular('tables'))
+      .is('table')
+      .string(toSingular('chairs'))
+      .is('chair')
+      .string(toSingular('cars'))
+      .is('car')
+      .string(toSingular('keys'))
+      .is('key');
+  }); // end it
+}); // end describe #toSingular
