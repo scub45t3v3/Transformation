7fde5a46fa8fd85b49695ac887e08542613eaae2
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:19:14 +0700

    remove iife from toRegExp.js

--- toRegExp.js
+++ toRegExp.js
@@ -1,27 +1,25 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toRegExp');
-  const toString = require('./toString');
-  const REGEX = /^([`~!@#$%^&*+=|\\?/])(.+)\1([gimuy]*)$/;
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toRegExp');
+const toString = require('./toString');
+const REGEX = /^([`~!@#$%^&*+=|\\?/])(.+)\1([gimuy]*)$/;
 
-  const toRegExp = (value) => {
-    debug('call:toRegExp(%o)', value);
+const toRegExp = (value) => {
+  debug('call:toRegExp(%o)', value);
 
-    if (value instanceof RegExp) {
-      return value;
-    }
+  if (value instanceof RegExp) {
+    return value;
+  }
 
-    const match = toString(value).match(REGEX);
+  const match = toString(value).match(REGEX);
 
-    if (!(match && match[2])) {
-      throw new TypeError(`${value} can not be transformed to a RegExp`);
-    }
+  if (!(match && match[2])) {
+    throw new TypeError(`${value} can not be transformed to a RegExp`);
+  }
 
-    return new RegExp(match[2], match[3]);
-  };
+  return new RegExp(match[2], match[3]);
+};
 
-  // export toRegExp as commonjs module
-  module.exports = toRegExp;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toRegExp;
