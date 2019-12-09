ea6f10cfe12cffc1a312a2d5f2b22c0c3210a990
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:20:17 +0700

    remove iife from toWords.js

--- toWords.js
+++ toWords.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toWords');
-  const toTokens = require('./toTokens');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toWords');
+const toTokens = require('./toTokens');
 
-  const toWords = (value) => {
-    debug('call:toWords(%o)', value);
+const toWords = (value) => {
+  debug('call:toWords(%o)', value);
 
-    return toTokens(value, /[!?.:;]*[\s,]+/);
-  }; // end toWords
+  return toTokens(value, /[!?.:;]*[\s,]+/);
+}; // end toWords
 
-  // export toWords as commonjs module
-  module.exports = toWords;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toWords;
