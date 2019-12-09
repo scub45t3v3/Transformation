64baf65c94dc5fd83fa91ea9831fee377ccf2aa7
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:17:13 +0700

    remove iife from toCSV.js

--- toCSV.js
+++ toCSV.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toCSV');
-  const {parse} = require('json2csv');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toCSV');
+const {parse} = require('json2csv');
 
-  const toCSV = (value, opt = {}) => {
-    debug('call:toCSV(%o, %o)', value, opt);
+const toCSV = (value, opt = {}) => {
+  debug('call:toCSV(%o, %o)', value, opt);
 
-    return parse(value, opt);
-  };
+  return parse(value, opt);
+};
 
-  // export toCSV as commonjs module
-  module.exports = toCSV;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toCSV;
