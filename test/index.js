85fe9d137347ecd8436b48a181f7d43e3df80b6c
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:02:16 +0700

    remove iife from test/index.js

--- test/index.js
+++ test/index.js
@@ -1,47 +1,45 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const index = require('../index');
-  const FUNCTIONS = [
-    'toArray',
-    'toAscii',
-    'toBase64',
-    'toBoolean',
-    'toCamelCase',
-    'toCSV',
-    'toFormat',
-    'toJSON',
-    'toKebabCase',
-    'toLines',
-    'toList',
-    'toListSerial',
-    'toMD5',
-    'toNumber',
-    'toPascalCase',
-    'toPlural',
-    'toRegExp',
-    'toSingular',
-    'toSlug',
-    'toSnakeCase',
-    'toString',
-    'toTokens',
-    'toWords',
-    'toWrap',
-    'toXML',
-    'toYAML',
-  ];
+// include dependencies
+const unit = require('unit.js');
+const index = require('../index');
+const FUNCTIONS = [
+  'toArray',
+  'toAscii',
+  'toBase64',
+  'toBoolean',
+  'toCamelCase',
+  'toCSV',
+  'toFormat',
+  'toJSON',
+  'toKebabCase',
+  'toLines',
+  'toList',
+  'toListSerial',
+  'toMD5',
+  'toNumber',
+  'toPascalCase',
+  'toPlural',
+  'toRegExp',
+  'toSingular',
+  'toSlug',
+  'toSnakeCase',
+  'toString',
+  'toTokens',
+  'toWords',
+  'toWrap',
+  'toXML',
+  'toYAML',
+];
 
-  // describe @scuba-squad/transformation
-  describe('@scuba-squad/transformation', () => {
-    it('should have several functions', () => {
-      unit
-        .object(index)
-        .hasProperties(FUNCTIONS)
-        .matchEach((value, key) => {
-          return typeof value === 'function' && /^to[A-Z]/.test(key);
-        });
-    }); // end it
-  }); // end describe @scuba-squad/transformation
-})(); // end IIFE
+// describe @scuba-squad/transformation
+describe('@scuba-squad/transformation', () => {
+  it('should have several functions', () => {
+    unit
+      .object(index)
+      .hasProperties(FUNCTIONS)
+      .matchEach((value, key) => {
+        return typeof value === 'function' && /^to[A-Z]/.test(key);
+      });
+  }); // end it
+}); // end describe @scuba-squad/transformation
