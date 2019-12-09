f8d4fdf64c9b5bb455ae0743a0ba490cb5244926
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:20:44 +0700

    remove iife from toYAML.js

--- toYAML.js
+++ toYAML.js
@@ -1,19 +1,17 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toYAML');
-  const yaml = require('js-yaml');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toYAML');
+const yaml = require('js-yaml');
 
-  const toYAML = (value, opt = {}) => {
-    debug('call:toYAML(%o, %o)', value, opt);
-    opt = Object.assign({
-      skipInvalid: true,
-    }, opt);
+const toYAML = (value, opt = {}) => {
+  debug('call:toYAML(%o, %o)', value, opt);
+  opt = Object.assign({
+    skipInvalid: true,
+  }, opt);
 
-    return yaml.safeDump(value, opt);
-  }; // end toYAML
+  return yaml.safeDump(value, opt);
+}; // end toYAML
 
-  // export toYAML as commonjs module
-  module.exports = toYAML;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toYAML;
