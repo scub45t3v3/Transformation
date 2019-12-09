21f16a295154cec720013fdbb9b44ab2da3d9036
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:16:30 +0700

    remove iife from index.js

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 'use strict';
 
-(() => {
-  // Type
-  exports.toArray = require('./toArray');
-  exports.toBoolean = require('./toBoolean');
-  exports.toNumber = require('./toNumber');
-  exports.toRegExp = require('./toRegExp');
-  exports.toString = require('./toString');
+// Type
+exports.toArray = require('./toArray');
+exports.toBoolean = require('./toBoolean');
+exports.toNumber = require('./toNumber');
+exports.toRegExp = require('./toRegExp');
+exports.toString = require('./toString');
 
-  // String format
-  exports.toAscii = require('./toAscii');
-  exports.toBase64 = require('./toBase64');
-  exports.toCamelCase = require('./toCamelCase');
-  exports.toFormat = require('./toFormat');
-  exports.toKebabCase = require('./toKebabCase');
-  exports.toLines = require('./toLines');
-  exports.toList = require('./toList');
-  exports.toListSerial = require('./toListSerial');
-  exports.toMD5 = require('./toMD5');
-  exports.toPascalCase = require('./toPascalCase');
-  exports.toPlural = require('./toPlural');
-  exports.toSingular = require('./toSingular');
-  exports.toSlug = require('./toSlug');
-  exports.toSnakeCase = require('./toSnakeCase');
-  exports.toTokens = require('./toTokens');
-  exports.toWords = require('./toWords');
-  exports.toWrap = require('./toWrap');
+// String format
+exports.toAscii = require('./toAscii');
+exports.toBase64 = require('./toBase64');
+exports.toCamelCase = require('./toCamelCase');
+exports.toFormat = require('./toFormat');
+exports.toKebabCase = require('./toKebabCase');
+exports.toLines = require('./toLines');
+exports.toList = require('./toList');
+exports.toListSerial = require('./toListSerial');
+exports.toMD5 = require('./toMD5');
+exports.toPascalCase = require('./toPascalCase');
+exports.toPlural = require('./toPlural');
+exports.toSingular = require('./toSingular');
+exports.toSlug = require('./toSlug');
+exports.toSnakeCase = require('./toSnakeCase');
+exports.toTokens = require('./toTokens');
+exports.toWords = require('./toWords');
+exports.toWrap = require('./toWrap');
 
-  // Serialize
-  exports.toCSV = require('./toCSV');
-  exports.toJSON = require('./toJSON');
-  exports.toXML = require('./toXML');
-  exports.toYAML = require('./toYAML');
-})(); // end IIFE
+// Serialize
+exports.toCSV = require('./toCSV');
+exports.toJSON = require('./toJSON');
+exports.toXML = require('./toXML');
+exports.toYAML = require('./toYAML');
