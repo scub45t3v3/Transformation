1d952485ff6f366049b8f0d62e7301cd48677413
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:18:01 +0700

    remove iife from toLines.js

--- toLines.js
+++ toLines.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toLines');
-  const toTokens = require('./toTokens');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toLines');
+const toTokens = require('./toTokens');
 
-  const toLines = (value) => {
-    debug('call:toLines(%o)', value);
+const toLines = (value) => {
+  debug('call:toLines(%o)', value);
 
-    return toTokens(value, /\r\n?|\n/gm);
-  };
+  return toTokens(value, /\r\n?|\n/gm);
+};
 
-  // export toLines as commonjs module
-  module.exports = toLines;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toLines;
