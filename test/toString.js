bd02385c0c2ae1f6ed5cd38ba1d9cacac9cbd285
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:06:24 +0700

    remove iife from test/toString.js

--- test/toString.js
+++ test/toString.js
@@ -1,25 +1,23 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const toString = require('../toString');
+// include dependencies
+const unit = require('unit.js');
+const toString = require('../toString');
 
-  // describe #toString
-  describe('#toString', () => {
-    it('should be a function', () => {
-      unit
-        .function(toString);
-    }); // end it
+// describe #toString
+describe('#toString', () => {
+  it('should be a function', () => {
+    unit
+      .function(toString);
+  }); // end it
 
-    it('should return a string', () => {
-      unit
-        .string(toString([1, 2, 3]))
-        .is('1,2,3')
-        .string(toString({a: 5}))
-        .is('[object Object]')
-        .string(toString(Object.create(null)))
-        .is('');
-    }); // end it
-  }); // end describe #toString
-})(); // end IIFE
+  it('should return a string', () => {
+    unit
+      .string(toString([1, 2, 3]))
+      .is('1,2,3')
+      .string(toString({a: 5}))
+      .is('[object Object]')
+      .string(toString(Object.create(null)))
+      .is('');
+  }); // end it
+}); // end describe #toString
