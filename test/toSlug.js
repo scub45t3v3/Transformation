590a3f3a56b18b5a53e6c1d38b8329f2caa69231
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:06:07 +0700

    remove iife from test/toSlug.js

--- test/toSlug.js
+++ test/toSlug.js
@@ -1,33 +1,31 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const toSlug = require('../toSlug');
+// include dependencies
+const unit = require('unit.js');
+const toSlug = require('../toSlug');
 
-  // describe #toSlug
-  describe('#toSlug', () => {
-    it('should be a function', () => {
-      unit
-        .function(toSlug);
-    }); // end it
+// describe #toSlug
+describe('#toSlug', () => {
+  it('should be a function', () => {
+    unit
+      .function(toSlug);
+  }); // end it
 
-    it('should return a slug when given a string', () => {
-      unit
-        .string(toSlug('hello world'))
-        .is('hello-world')
-        .string(toSlug('helloWorld'))
-        .is('hello-World')
-        .string(toSlug('＄ vs €'))
-        .is('dollar-vs-euro');
-    }); // end it
+  it('should return a slug when given a string', () => {
+    unit
+      .string(toSlug('hello world'))
+      .is('hello-world')
+      .string(toSlug('helloWorld'))
+      .is('hello-World')
+      .string(toSlug('＄ vs €'))
+      .is('dollar-vs-euro');
+  }); // end it
 
-    it('should return a slug when given an array', () => {
-      unit
-        .string(toSlug(['hello', 'world']))
-        .is('hello-world')
-        .string(toSlug(['＄', 'vs', '€']))
-        .is('dollar-vs-euro');
-    }); // end it
-  }); // end describe #toSlug
-})(); // end IIFE
+  it('should return a slug when given an array', () => {
+    unit
+      .string(toSlug(['hello', 'world']))
+      .is('hello-world')
+      .string(toSlug(['＄', 'vs', '€']))
+      .is('dollar-vs-euro');
+  }); // end it
+}); // end describe #toSlug
