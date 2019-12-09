fa1dee73f3282211162e0c82629b7b68c8728fe6
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:04:03 +0700

    remove iife from test/toLines.js

--- test/toLines.js
+++ test/toLines.js
@@ -1,56 +1,54 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const toLines = require('../toLines');
+// include dependencies
+const unit = require('unit.js');
+const toLines = require('../toLines');
 
-  // describe #toLines
-  describe('#toLines', () => {
-    it('should be a function', () => {
-      unit
-        .function(toLines);
-    }); // end it
+// describe #toLines
+describe('#toLines', () => {
+  it('should be a function', () => {
+    unit
+      .function(toLines);
+  }); // end it
 
-    it('should return an array with the string for strings without CRLF|CR|LF', () => {
-      unit
-        .array(toLines('123'))
-        .is(['123'])
-        .array(toLines('asd'))
-        .is(['asd']);
-    }); // end it
+  it('should return an array with the string for strings without CRLF|CR|LF', () => {
+    unit
+      .array(toLines('123'))
+      .is(['123'])
+      .array(toLines('asd'))
+      .is(['asd']);
+  }); // end it
 
-    it('should return an array with a default joined string for arrays without CRLF|CR|LF', () => {
-      unit
-        .array(toLines(['a', 'b', 'c']))
-        .is(['a,b,c']);
-    }); // end it
+  it('should return an array with a default joined string for arrays without CRLF|CR|LF', () => {
+    unit
+      .array(toLines(['a', 'b', 'c']))
+      .is(['a,b,c']);
+  }); // end it
 
-    it('should return an array of lines for strings with CRLF|CR|LF', () => {
-      unit
-        .array(toLines('a\nb\nc'))
-        .is(['a', 'b', 'c'])
-        .array(toLines('\n\r\n\n1\r\n\r2'))
-        .is([
-          '',
-          '',
-          '',
-          '1',
-          '',
-          '2',
-        ]);
-    }); // end it
+  it('should return an array of lines for strings with CRLF|CR|LF', () => {
+    unit
+      .array(toLines('a\nb\nc'))
+      .is(['a', 'b', 'c'])
+      .array(toLines('\n\r\n\n1\r\n\r2'))
+      .is([
+        '',
+        '',
+        '',
+        '1',
+        '',
+        '2',
+      ]);
+  }); // end it
 
-    it('should return an array of lines for arrays with CRLF|CR|LF', () => {
-      unit
-        .array(toLines(['\na\r\n', '\r5\n']))
-        .is([
-          '',
-          'a',
-          ',',
-          '5',
-          '',
-        ]);
-    }); // end it
-  }); // end describe #toLines
-})(); // end IIFE
+  it('should return an array of lines for arrays with CRLF|CR|LF', () => {
+    unit
+      .array(toLines(['\na\r\n', '\r5\n']))
+      .is([
+        '',
+        'a',
+        ',',
+        '5',
+        '',
+      ]);
+  }); // end it
+}); // end describe #toLines
