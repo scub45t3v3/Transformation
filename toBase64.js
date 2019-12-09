bd0e2da229b890250ac33ee7d89ce4a4951879e1
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:16:56 +0700

    remove iife from toBase64.js

--- toBase64.js
+++ toBase64.js
@@ -1,21 +1,19 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toBase64');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toBase64');
 
-  const toBase64 = (value) => {
-    debug('call:toBase64(%o)', value);
+const toBase64 = (value) => {
+  debug('call:toBase64(%o)', value);
 
-    if (!(value instanceof String || typeof value === 'string')) {
-      value = JSON.stringify(value);
-    }
+  if (!(value instanceof String || typeof value === 'string')) {
+    value = JSON.stringify(value);
+  }
 
-    return Buffer
-      .from(value)
-      .toString('base64');
-  };
+  return Buffer
+    .from(value)
+    .toString('base64');
+};
 
-  // export as commonjs module
-  module.exports = toBase64;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toBase64;
