005555ba95d141126fe0219370366f28a5dfccb9
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:06:46 +0700

    remove iife from test/toWords.js

--- test/toWords.js
+++ test/toWords.js
@@ -1,33 +1,31 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const toWords = require('../toWords');
+// include dependencies
+const unit = require('unit.js');
+const toWords = require('../toWords');
 
-  // describe #toWords
-  describe('#toWords', () => {
-    it('should be a function', () => {
-      unit
-        .function(toWords);
-    }); // end it
+// describe #toWords
+describe('#toWords', () => {
+  it('should be a function', () => {
+    unit
+      .function(toWords);
+  }); // end it
 
-    it('should return an array of words in a string', () => {
-      unit
-        .array(toWords('hello world!'))
-        .is(['hello', 'world!'])
-        .array(toWords('123 Main St.'))
-        .is(['123', 'Main', 'St.'])
-        .array(toWords('this is $1.00'))
-        .is(['this', 'is', '$1.00'])
-        .array(toWords('a,b,c'))
-        .is(['a', 'b', 'c'])
-        .array(toWords('fruit: apples, oranges, bananas'))
-        .is(['fruit', 'apples', 'oranges', 'bananas'])
-        .array(toWords('one'))
-        .is(['one'])
-        .array(toWords(''))
-        .is(['']);
-    }); // end it
-  }); // end describe #toWords
-})(); // end IIFE
+  it('should return an array of words in a string', () => {
+    unit
+      .array(toWords('hello world!'))
+      .is(['hello', 'world!'])
+      .array(toWords('123 Main St.'))
+      .is(['123', 'Main', 'St.'])
+      .array(toWords('this is $1.00'))
+      .is(['this', 'is', '$1.00'])
+      .array(toWords('a,b,c'))
+      .is(['a', 'b', 'c'])
+      .array(toWords('fruit: apples, oranges, bananas'))
+      .is(['fruit', 'apples', 'oranges', 'bananas'])
+      .array(toWords('one'))
+      .is(['one'])
+      .array(toWords(''))
+      .is(['']);
+  }); // end it
+}); // end describe #toWords
