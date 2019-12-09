ef56c968870bc8a7b74a32140fca33b6a20401d0
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:03:16 +0700

    remove iife from test/toCSV.js

--- test/toCSV.js
+++ test/toCSV.js
@@ -4,29 +4,27 @@
  * this is a direct passthrough to json2csv all tests should be preformed on
  * json2csv this is only included to test the passthrough is setup as expected
  */
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const toCSV = require('../toCSV');
+// include dependencies
+const unit = require('unit.js');
+const toCSV = require('../toCSV');
 
-  describe('#toCSV', () => {
-    it('should be a function', () => {
-      unit
-        .function(toCSV);
-    }); // end it
+describe('#toCSV', () => {
+  it('should be a function', () => {
+    unit
+      .function(toCSV);
+  }); // end it
 
-    it('should return a csv string for an object', () => {
-      const test = {
-        firstName: 'James',
-        lastName: 'Bond',
-        age: 55,
-      };
+  it('should return a csv string for an object', () => {
+    const test = {
+      firstName: 'James',
+      lastName: 'Bond',
+      age: 55,
+    };
 
-      const regex = /^\s*"firstName"\s*,\s*"lastName"\s*,\s*"age"\s*"James"\s*,\s*"Bond"\s*,\s*55\s*$/m;
+    const regex = /^\s*"firstName"\s*,\s*"lastName"\s*,\s*"age"\s*"James"\s*,\s*"Bond"\s*,\s*55\s*$/m;
 
-      unit
-        .string(toCSV(test))
-        .match(regex);
-    }); // end it
-  }); // end describe #toCSV
-})(); // end IIFE
+    unit
+      .string(toCSV(test))
+      .match(regex);
+  }); // end it
+}); // end describe #toCSV
