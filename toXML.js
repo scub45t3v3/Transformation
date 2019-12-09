2abfbad90d55f3ac140332eb4a5e3d467432cde7
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:20:35 +0700

    remove iife from toXML.js

--- toXML.js
+++ toXML.js
@@ -1,21 +1,19 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toXML');
-  const xml = require('xml-js');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toXML');
+const xml = require('xml-js');
 
-  const toXML = (value, opt = {}) => {
-    debug('call:toXML(%o, %o)', value, opt);
-    opt = Object.assign({
-      spaces: 2,
-      compact: true,
-      indentCdata: true,
-    }, opt);
+const toXML = (value, opt = {}) => {
+  debug('call:toXML(%o, %o)', value, opt);
+  opt = Object.assign({
+    spaces: 2,
+    compact: true,
+    indentCdata: true,
+  }, opt);
 
-    return xml.json2xml(value, opt);
-  }; // end toXML
+  return xml.json2xml(value, opt);
+}; // end toXML
 
-  // export toXML as commonjs module
-  module.exports = toXML;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toXML;
