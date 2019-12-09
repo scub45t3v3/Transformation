df46fc79698598d41e06a8c69affb7ad1b4c28bd
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:17:41 +0700

    remove iife from toJSON.js

--- toJSON.js
+++ toJSON.js
@@ -1,15 +1,13 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toJSON');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toJSON');
 
-  const toJSON = (value, space = 2) => {
-    debug('call:toJSON(%o, %o)', value, space);
+const toJSON = (value, space = 2) => {
+  debug('call:toJSON(%o, %o)', value, space);
 
-    return JSON.stringify(value, null, space);
-  };
+  return JSON.stringify(value, null, space);
+};
 
-  // export toJSON as commonjs module
-  module.exports = toJSON;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toJSON;
