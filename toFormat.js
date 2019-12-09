eec1a78a821b08b0d1fa3483cb56152702909b23
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:17:34 +0700

    remove iife from toFormat.js

--- toFormat.js
+++ toFormat.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toFormat');
-  const {format} = require('util');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toFormat');
+const {format} = require('util');
 
-  const toFormat = (...args) => {
-    debug('call:toFormat(%o)', args);
+const toFormat = (...args) => {
+  debug('call:toFormat(%o)', args);
 
-    return format(...args);
-  };
+  return format(...args);
+};
 
-  // export toFormat as commonjs module
-  module.exports = toFormat;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toFormat;
