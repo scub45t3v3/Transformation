70d44bd78c21caa273ffe93ca9c2ccb826f24dee
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:19:36 +0700

    remove iife from toSlug.js

--- toSlug.js
+++ toSlug.js
@@ -1,34 +1,32 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toSlug');
-  const currencies = require('./util/currencies');
-  const diacritics = require('./util/diacritics');
-  const symbols = require('./util/symbols');
-  const toString = require('./toString');
-  const charMap = Object.assign({}, currencies, diacritics, symbols);
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toSlug');
+const currencies = require('./util/currencies');
+const diacritics = require('./util/diacritics');
+const symbols = require('./util/symbols');
+const toString = require('./toString');
+const charMap = Object.assign({}, currencies, diacritics, symbols);
 
-  const toSlug = (value) => {
-    debug('call:toSlug(%o)', value);
+const toSlug = (value) => {
+  debug('call:toSlug(%o)', value);
 
-    if (Array.isArray(value)) {
-      value = value.join(' ');
-    }
+  if (Array.isArray(value)) {
+    value = value.join(' ');
+  }
 
-    return toString(value)
-      .split('')
-      .map((char) => {
-        return charMap[char] || char;
-      })
-      .join('')
-      .trim()
-      .replace(/[\s_-]+/g, '-')
-      .replace(/^-+|-+$/g, '')
-      .replace(/([a-z])([A-Z])/gm, '$1-$2')
-      .replace(/[^\w.\-~!$&'()*+,;=':@%]+/, '');
-  };
+  return toString(value)
+    .split('')
+    .map((char) => {
+      return charMap[char] || char;
+    })
+    .join('')
+    .trim()
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+    .replace(/([a-z])([A-Z])/gm, '$1-$2')
+    .replace(/[^\w.\-~!$&'()*+,;=':@%]+/, '');
+};
 
-  // export toSlug as commonjs module
-  module.exports = toSlug;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toSlug;
