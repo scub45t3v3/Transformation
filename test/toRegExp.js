c8b92156665f6b5e1782dccee64fdaeceafd5cd8
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:05:46 +0700

    remove iife from test/toRegExp.js

--- test/toRegExp.js
+++ test/toRegExp.js
@@ -1,47 +1,45 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const toRegExp = require('../toRegExp');
+// include dependencies
+const unit = require('unit.js');
+const toRegExp = require('../toRegExp');
 
-  // describe #toRegExp
-  describe('#toRegExp', () => {
-    it('should be a function', () => {
-      unit.function(toRegExp);
-    }); // end it
+// describe #toRegExp
+describe('#toRegExp', () => {
+  it('should be a function', () => {
+    unit.function(toRegExp);
+  }); // end it
 
-    it('should return a RegExp when given a RegExp object', () => {
-      unit
-        .regexp(toRegExp(/asd/i))
-        .is(/asd/i)
-        .regexp(toRegExp(new RegExp('^hello', 'i')))
-        .is(/^hello/i)
-        .regexp(toRegExp(/^\d+$/))
-        .is(/^\d+$/);
-    }); // end it
+  it('should return a RegExp when given a RegExp object', () => {
+    unit
+      .regexp(toRegExp(/asd/i))
+      .is(/asd/i)
+      .regexp(toRegExp(new RegExp('^hello', 'i')))
+      .is(/^hello/i)
+      .regexp(toRegExp(/^\d+$/))
+      .is(/^\d+$/);
+  }); // end it
 
-    it('should return a RegExp object when given a serialized pcre string', () => {
-      unit
-        .regexp(toRegExp('/asd/i'))
-        .is(/asd/i)
-        .regexp(toRegExp('%^hello%i'))
-        .is(/^hello/i)
-        .regexp(toRegExp('|\\d+|g'))
-        .is(/\d+/g);
-    }); // end it
+  it('should return a RegExp object when given a serialized pcre string', () => {
+    unit
+      .regexp(toRegExp('/asd/i'))
+      .is(/asd/i)
+      .regexp(toRegExp('%^hello%i'))
+      .is(/^hello/i)
+      .regexp(toRegExp('|\\d+|g'))
+      .is(/\d+/g);
+  }); // end it
 
-    it('should throw an error when given a non-serilized pcre string', () => {
-      unit
-        .error(() => {
-          toRegExp('hello');
-        })
-        .error(() => {
-          toRegExp('/asd|i');
-        })
-        .error(() => {
-          toRegExp('/asd/t');
-        });
-    }); // end it
-  }); // end describe #toRegExp
-})(); // end IIFE
+  it('should throw an error when given a non-serilized pcre string', () => {
+    unit
+      .error(() => {
+        toRegExp('hello');
+      })
+      .error(() => {
+        toRegExp('/asd|i');
+      })
+      .error(() => {
+        toRegExp('/asd/t');
+      });
+  }); // end it
+}); // end describe #toRegExp
