b2194701dce25b15837ffd3b39466b2498155594
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:18:53 +0700

    remove iife from toPascalCase.js

--- toPascalCase.js
+++ toPascalCase.js
@@ -1,26 +1,24 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toPascalCase');
-  const toString = require('./toString');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toPascalCase');
+const toString = require('./toString');
 
-  const toPascalCase = (value) => {
-    debug('call:toPascalCase(%o)', value);
+const toPascalCase = (value) => {
+  debug('call:toPascalCase(%o)', value);
 
-    return toString(value)
-      .trim()
-      .replace(/(?:\W|_)+/gm, '_')
-      .replace(/^_+/, '')
-      .replace(/_+$/, '')
-      .replace(/_(.)/gm, (match, char) => {
-        return char && char.toUpperCase();
-      })
-      .replace(/^[a-z]/, (match) => {
-        return match && match.toUpperCase();
-      });
-  }; // end toPascalCase
+  return toString(value)
+    .trim()
+    .replace(/(?:\W|_)+/gm, '_')
+    .replace(/^_+/, '')
+    .replace(/_+$/, '')
+    .replace(/_(.)/gm, (match, char) => {
+      return char && char.toUpperCase();
+    })
+    .replace(/^[a-z]/, (match) => {
+      return match && match.toUpperCase();
+    });
+}; // end toPascalCase
 
-  // export toPascalCase as commonjs module
-  module.exports = toPascalCase;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toPascalCase;
