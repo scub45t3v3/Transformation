86289b8922b08054008dbf3acfbc490207960f2d
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:20:06 +0700

    remove iife from toTokens.js

--- toTokens.js
+++ toTokens.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toTokens');
-  const toString = require('./toString');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toTokens');
+const toString = require('./toString');
 
-  const toTokens = (value, delimiter = /\s+/) => {
-    debug('call:toTokens(%o, %o)', value, delimiter);
+const toTokens = (value, delimiter = /\s+/) => {
+  debug('call:toTokens(%o, %o)', value, delimiter);
 
-    return toString(value).split(delimiter);
-  }; // end toTokens
+  return toString(value).split(delimiter);
+}; // end toTokens
 
-  // expor toTokens as commonjs module
-  module.exports = toTokens;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toTokens;
