3abe2a79106b5eacf0304311f30adb9a51bfce2e
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:20:27 +0700

    remove iife from toWrap.js

--- toWrap.js
+++ toWrap.js
@@ -1,19 +1,17 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toWrap');
-  const toString = require('./toString');
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toWrap');
+const toString = require('./toString');
 
-  const toWrap = (value, opt = {}) => {
-    debug('call:toWrap(%o, %o)', value, opt);
-    value = toString(value);
-    opt.start = toString(opt.start) || toString(opt.end);
-    opt.end = toString(opt.end) || opt.start;
+const toWrap = (value, opt = {}) => {
+  debug('call:toWrap(%o, %o)', value, opt);
+  value = toString(value);
+  opt.start = toString(opt.start) || toString(opt.end);
+  opt.end = toString(opt.end) || opt.start;
 
-    return `${opt.start}${value}${opt.end}`;
-  }; // end toWrap
+  return `${opt.start}${value}${opt.end}`;
+}; // end toWrap
 
-  // export toWrap as commonjs module
-  module.exports = toWrap;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toWrap;
