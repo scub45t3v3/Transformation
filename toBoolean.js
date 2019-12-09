561462a304d96919372d72c93fb916cb6e01de78
Steve Winberg <stevewin21@gmail.com>
2019-12-09 17:17:05 +0700

    remove iife from toBoolean.js

--- toBoolean.js
+++ toBoolean.js
@@ -1,17 +1,15 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:transformation:toBoolean');
-  const toString = require('./toString');
-  const REGEX = /^(?:0+|-|null|undefined|NaN|off|f(?:alse)?|no(?:ne)?|neg(?:ative)?|)$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:transformation:toBoolean');
+const toString = require('./toString');
+const REGEX = /^(?:0+|-|null|undefined|NaN|off|f(?:alse)?|no(?:ne)?|neg(?:ative)?|)$/i;
 
-  const toBoolean = (value) => {
-    debug('call:toBoolean(%o)', value);
+const toBoolean = (value) => {
+  debug('call:toBoolean(%o)', value);
 
-    return !!value && !REGEX.test(toString(value).trim());
-  };
+  return !!value && !REGEX.test(toString(value).trim());
+};
 
-  // export toBoolean as commonjs module
-  module.exports = toBoolean;
-})(); // end IIFE
+// export as commonjs module
+module.exports = toBoolean;
